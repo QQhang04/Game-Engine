6c481a40748554c1e26e94615a4e59f30a2d7389
钱 启航 <1978980854@qq.com>
2025-06-10 15:27:06 +0800

    First SDL Window and Basic Skeleton

--- src/Game.cpp
+++ src/Game.cpp
@@ -3,6 +3,7 @@
 
 Game::Game() {
     std::cout << "Game initialized" << std::endl;
+    isRunning = false;
 }
 
 Game::~Game() {
@@ -10,11 +11,36 @@ Game::~Game() {
 }
 
 void Game::Initialize() {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return;
+    }
+
+    window = SDL_CreateWindow(
+        "QQh Game", 
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED, 
+        800, 
+        600, 
+        SDL_WINDOW_BORDERLESS
+    );
+    if (!window) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        return;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    if (!renderer) {
+        std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+        return;
+    }
+
+    isRunning = true;
 
 }
 
 void Game::Run() {
-    while (true) {
+    while (isRunning) {
         ProcessInput();
         Update();
         Render();
@@ -22,7 +48,19 @@ void Game::Run() {
 }
 
 void Game::ProcessInput() {
-
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_QUIT:
+                isRunning = false;
+                break;
+            case SDL_KEYDOWN:
+                if (event.key.keysym.sym == SDLK_ESCAPE) {
+                    isRunning = false;
+                }
+                break;
+        }
+    }
 }
 
 void Game::Update() {
@@ -30,9 +68,16 @@ void Game::Update() {
 }
 
 void Game::Render() {
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);
 
+    // TODO: Draw the game here
+
+    SDL_RenderPresent(renderer);
 }
 
 void Game::Destroy() {
-
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
